4441184718e2484a1eb4863e7cd8290a5dbc9227
Tella <tellarinipietro@gmail.com>
2022-05-11 14:53:33 +0200

    Create TowerTwo.java

---
+++ src/obsidia/entities/towers/TowerTwo.java
@@ -0,0 +1,28 @@
+package obsidia.entities.towers;
+
+
+public class TowerTwo extends EmptyCell implements Tower{
+
+	private static final int COST_LV2 = 25;
+	private static final int BALANCE_LV2 = -6;
+	private static final int DEFENCE_LV2 = 3;
+
+	public TowerTwo(String ownerID, Coordinates xy) {
+		super(ownerID, xy, TowerTwo.BALANCE_LV2, TowerTwo.DEFENCE_LV2);
+	}
+	
+	@Override
+	public int getCost() {
+		return TowerTwo.COST_LV2;
+	}
+
+	@Override
+	public int getMaintenance() {
+		return TowerTwo.BALANCE_LV2;
+	}
+
+	@Override
+	public int getDefenceLv() {
+		return TowerTwo.DEFENCE_LV2;
+	}
+}
