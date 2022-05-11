0023d7e77b0fe1fb08fbba79eb9c6e31bc7f6f7d
Tella <tellarinipietro@gmail.com>
2022-05-11 15:13:34 +0200

    Create Cells.java

---
+++ src/obsidia/entities/Cells.java
@@ -0,0 +1,15 @@
+package obsidia.entities;
+
+import obsidia.utilities.Coordinates;
+
+public interface Cells {
+
+	public int getBalance();
+
+	public int getDefence();
+		
+	public String getOwner();
+	
+	public Coordinates getCoordinates();
+	
+}
