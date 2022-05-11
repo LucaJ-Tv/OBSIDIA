a9c8cd51dcd9add9d9ac798a5963d6eec59c8ac6
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-05-11 14:08:12 +0200

    Create Farm.java

---
+++ src/obsidia/entities/buildings/Farm.java
@@ -0,0 +1,11 @@
+package obsidia.entities.buildings;
+
+public class Farm extends Building{
+
+	/*
+	 * Concrete Class Farm
+	 */
+	public Farm(int defenceLevel, int gain) {
+		super(defenceLevel, gain);
+	}
+}
