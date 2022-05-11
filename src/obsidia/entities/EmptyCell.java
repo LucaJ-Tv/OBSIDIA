318ef430df636b0da4d63bad79e4bb2a5e21af7a
Tella <tellarinipietro@gmail.com>
2022-05-11 15:13:41 +0200

    Create EmptyCell.java

---
+++ src/obsidia/entities/EmptyCell.java
@@ -0,0 +1,38 @@
+package obsidia.entities;
+
+import obsidia.utilities.Coordinates;
+
+public class EmptyCell implements Cells{
+
+	private final Coordinates xy;
+	private final String ownerID;
+	private final int balance;
+	private final int defence;
+	
+	public EmptyCell(final String ownerID, final Coordinates xy, final int balance, final int lvDefence) {
+		this.balance = balance;
+		this.ownerID = ownerID;
+		this.xy = xy;
+		this.defence = lvDefence;
+	}
+
+	@Override
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	@Override
+	public int getDefence() {
+		return this.defence;
+	}
+	
+	@Override
+	public String getOwner() {
+		return this.ownerID;
+	}
+
+	@Override
+	public Coordinates getCoordinates() {
+		return this.xy;
+	}
+}
