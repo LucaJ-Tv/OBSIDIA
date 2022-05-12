c52306b32f92d67ec6e6db451cddcc6adee94b61
Tella <tellarinipietro@gmail.com>
2022-05-12 19:56:52 +0200

    Create GameMap.java

---
+++ src/obsidia/map/GameMap.java
@@ -0,0 +1,39 @@
+package obsidia.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import obsidia.entities.Cells;
+import obsidia.utilities.Coordinates;
+
+public class GameMap {
+	
+	private final Map<Coordinates, Cells> GameMap = new HashMap<>();
+	
+	public void addEntity(final Cells entity) {
+		GameMap.put(entity.getCoordinates(), entity);
+	}
+	
+	public int getDefence(final Coordinates xy) {
+		return GameMap.get(xy).getDefence();
+	}
+
+	public String getOwner(final Coordinates xy) {
+		return GameMap.get(xy).getOwner();
+	}
+	
+	public int getIdBalance(final String ID){
+		int balance = 0;
+		for ( final var i : GameMap.values() ) {
+			if(i.getOwner() == ID) {
+				balance += i.getBalance();
+			}
+		}
+		return balance;
+	}
+	
+	public int getBalance(final Coordinates xy) {
+		return GameMap.get(xy).getBalance();
+	}
+
+}
