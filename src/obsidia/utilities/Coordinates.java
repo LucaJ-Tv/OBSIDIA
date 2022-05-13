d01ae6ba4c3a614775544b4c17765caa81db5091
Luca Ferar Tofan Nicolae <47209041+LucaJ-Tv@users.noreply.github.com>
2022-05-13 17:23:34 +0200

    add coordinates
    simple class useful for all cells.

--- src/obsidia/utilities/Coordinates.java
+++ src/obsidia/utilities/Coordinates.java
@@ -1,5 +1,23 @@
 package obsidia.utilities;
 
 public class Coordinates {
-
+	private final int x;
+	private final int y;
+	
+	public Coordinates(final int x, final int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public String toString() {
+		return "x: " + this.x  + "y: " + this.y;
+	}
 }
