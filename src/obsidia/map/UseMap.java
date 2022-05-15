2fb6209796ef70120f998d2bb88180543b42043c
Tella <tellarinipietro@gmail.com>
2022-05-15 20:42:18 +0200

    Renamed GameMap in UseMap.java
    it's create a static map

--- src/obsidia/map/UseMap.java
+++ src/obsidia/map/UseMap.java
@@ -6,33 +6,33 @@ import java.util.Map;
 import obsidia.entities.Cells;
 import obsidia.utilities.Coordinates;
 
-public class GameMap {
+public class UseMap {
 	
-	private final Map<Coordinates, Cells> map = new HashMap<>();
+	private static final Map<Coordinates, Cells> MAP = new HashMap<Coordinates, Cells>();
 	
 	public void addEntity(final Cells entity) {
-		this.map.put(entity.getCoordinates(), entity);
+		UseMap.MAP.put(entity.getCoordinates(), entity);
 	}
 	
 	public int getDefence(final Coordinates xy) {
-		return this.map.get(xy).getDefence();
+		return UseMap.MAP.get(xy).getDefence();
 	}
 
 	public String getOwner(final Coordinates xy) {
-		return this.map.get(xy).getOwner();
+		return UseMap.MAP.get(xy).getOwner();
 	}
 	
 	public Class<? extends Cells> getEntity(final Coordinates xy) {
-		return this.map.get(xy).getClass();
+		return UseMap.MAP.get(xy).getClass();
 	}
 	
 	public int getBalance(final Coordinates xy) {
-		return this.map.get(xy).getBalance();
+		return UseMap.MAP.get(xy).getBalance();
 	}
 	
 	public int getIdBalance(final String ID){
 		int balance = 0;
-		for ( final var i : this.map.values() ) {
+		for ( final var i : UseMap.MAP.values() ) {
 			if(i.getOwner() == ID) {
 				balance += i.getBalance();
 			}
