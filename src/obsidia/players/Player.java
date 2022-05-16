2692ada11cd77fee908c1f384f3754f046732585
Tella <tellarinipietro@gmail.com>
2022-05-16 12:19:50 +0200

    Created class Player and UsePlayer
    Player contains the data of the players.
    UsePlayer needed to provide all mathod to get the data from Player.
    The player are in a linked list managed by UsePlayer.java

---
+++ src/obsidia/players/Player.java
@@ -0,0 +1,42 @@
+package obsidia.players;
+
+import java.awt.Color;
+
+public class Player{
+	
+	private static final int DEFAULT_COINS = 10;
+	
+	private final String name;
+	private final Color color;
+	private int coins;
+	
+	
+	public Player(final String name, final Color color, final int startCoins) {
+		this.name = name;
+		this.color = color;
+		this.coins = startCoins;
+		UsePlayer.addPlayer(this);		
+	}
+	
+	public Player(final String player, final Color color) {
+		this(player, color, Player.DEFAULT_COINS);
+	}
+	
+	protected String getName() {
+		return name;
+	}
+	
+	protected Color getColor() {
+		return color;
+	}
+
+	protected int getCoins() {
+		return coins;
+	}
+
+	protected void setCoins(int coins) {
+		this.coins = coins;
+	}
+	
+	
+}
---
+++ src/obsidia/players/UsePlayer.java
@@ -0,0 +1,42 @@
+package obsidia.players;
+
+import java.awt.Color;
+import java.util.LinkedList;
+
+public class UsePlayer {
+	
+	private static final LinkedList<Player> PLY = new LinkedList<>();
+	
+	public static void addPlayer(final Player p) {
+		PLY.addLast(p);
+	}
+
+	public String getName() {
+		return PLY.getFirst().getName();
+	}
+	
+	public int getCoins() {
+		return PLY.getFirst().getCoins();
+	}
+	
+	public Color getColor() {
+		return PLY.getFirst().getColor();		
+	}
+	
+	public void setCoins(final int coins) {
+		PLY.getFirst().setCoins(coins);
+	}
+	
+	public void nextPlayer() {
+		final Player p = PLY.removeFirst();
+		addPlayer(p);
+	}
+	
+	public void removePlayer(final String name) {
+		for (var i : PLY) {
+			if(i.getName() == name) {
+				PLY.remove(i);
+			}
+		}
+	}
+}
