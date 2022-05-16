66a2840984278f31e02b5911c097fa207ef2211b
Tella <tellarinipietro@gmail.com>
2022-05-16 15:06:35 +0200

    Update UseMap.java
    Created the numberFarm(String) method. It returns the number of farm of given player name.
    addEntity(Cells) it's now protected. It can be only called when you create a new entities (Cells type).

--- src/obsidia/map/UseMap.java
+++ src/obsidia/map/UseMap.java
@@ -3,13 +3,14 @@ package obsidia.map;
 import java.util.HashMap;
 import java.util.Map;
 
+import obsidia.entities.buildings.Farm;
 import obsidia.utilities.Coordinates;
 
 public class UseMap {
 	
 	private static final Map<Coordinates, Cells> MAP = new HashMap<Coordinates, Cells>();
 	
-	public static void addEntity(final Cells entity) {
+	protected static void addEntity(final Cells entity) {
 		UseMap.MAP.put(entity.getCoordinates(), entity);
 	}
 	
@@ -30,13 +31,13 @@ public class UseMap {
 	}
 	
 	public int numberFarm(final String ID) {
-		int balance = 0;
-		for ( final var i : UseMap.MAP.values() ) {
-			if(i.getOwner() == ID) {
-				balance += i.getBalance();
+		int num = 0;
+		for ( final Cells i : UseMap.MAP.values() ) {
+			if(i instanceof Farm) {
+				num ++;
 			}
 		}
-		return balance;		
+		return num;		
 	}
 	
 	public int getIdBalance(final String ID){
