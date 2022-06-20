466e6320c80ebf9cd0ea050c9bf276ed92529958
Tella <tellarinipietro@gmail.com>
2022-06-20 15:48:48 +0200

    Update TroopMovement.java
    Add methods

--- src/logic/troop/TroopMovement.java
+++ src/logic/troop/TroopMovement.java
@@ -3,11 +3,16 @@ package logic.troop;
 import obsidia.entities.cells.FreeCell;
 import obsidia.entities.troops.Troops;
 import obsidia.map.UseMap;
+import obsidia.players.PlayerList;
 import obsidia.utilities.Coordinates;
 
 public class TroopMovement {
 	
 	private final UseMap mp = new UseMap();
+	
+	private boolean isMyBuilding(Coordinates pos, Troops troop) {
+		return troop.getOwner() == mp.getOwner(pos);
+	}
 	
 	private boolean Stronger(Coordinates pos, Troops troop) {
 		return troop.getOffence() >= mp.getDefence(pos);
@@ -19,8 +24,19 @@ public class TroopMovement {
 	}
 
 	public boolean isConquerable(Coordinates pos, Troops troop) {
-		return (Stronger(pos, troop) && inRange(pos, troop));
+		return (Stronger(pos, troop) && inRange(pos, troop) && !isMyBuilding(pos, troop));
 		
+	}
+	
+	public Coordinates[] allConquerable(Troops troop) {
+		Coordinates[] cords = {null};
+		int j = 0;
+		for (Coordinates i : troop.getCoordinates().near(troop.getMovement(), mp.getWidth(), mp.getHeight())) {
+			if(this.isConquerable(i, troop)) {
+				cords[j++] = i;
+			}
+		}
+		return cords;	
 	}
 	
 	public void posConquest(Coordinates pos, Troops troop) {
