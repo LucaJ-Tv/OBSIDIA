96c824430fb7c7ebc010a79e8a541dd6a597ee6c
Tella <tellarinipietro@gmail.com>
2022-06-20 15:41:36 +0200

    Update Coordinates.java
    Add method near (give the cells near to another in the range)
    Add method distance (give distance between cells)

--- src/obsidia/utilities/Coordinates.java
+++ src/obsidia/utilities/Coordinates.java
@@ -1,5 +1,7 @@
 package obsidia.utilities;
 
+import java.util.Set;
+
 public class Coordinates {
 	private final int x;
 	private final int y;
@@ -21,13 +23,45 @@ public class Coordinates {
 		return "x: " + this.x  + "y: " + this.y;
 	}
 	
-	public int distance(Coordinates other) {
-		final int Dx = other.getX() - this.getX();
-		final int Dy = other.getY() - this.getY();
+	public int distance(final Coordinates other) {
+		final int Dx = Math.abs(other.getX() - this.getX());
+		final int Dy = Math.abs(other.getY() - this.getY());
 		
 		if (Dx >= Dy)
 			return Dx;
 		else
 			return Dy;
 	}
+	
+	public Coordinates[] near(final int distance, final int mapWidth, final int mapHeight) {
+		
+		Set<Integer> set = Set.of(0);
+		Coordinates[] cords = {};
+		int nx;
+		int ny;
+		int ni = 0;
+		
+		for(int i = 1; i < distance+1; i++) {
+			set.add(i);
+			set.add(-i);
+		}
+		
+		for(int i : set) {
+			nx = this.x + i;
+			for(int j : set) {
+				ny = this.y + j;
+				if(		nx >= 0 &&
+						nx < mapWidth &&
+						ny >= 0 &&
+						ny < mapHeight &&
+						(nx == this.x && ny == this.y)
+					) {
+					cords[ni++] = new Coordinates(nx, ny);
+				}
+			}
+		}
+		return cords;
+	}
+	
+	
 }
