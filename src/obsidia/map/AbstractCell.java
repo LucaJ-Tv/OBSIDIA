381eebeb28d32c34456b9dcb17267b9111ea20d2
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-21 13:08:34 +0200

    Update AbstractCell.java
    add path field for entity icon

--- src/obsidia/map/AbstractCell.java
+++ src/obsidia/map/AbstractCell.java
@@ -1,11 +1,14 @@
 package obsidia.map;
 
+import java.nio.file.Path;
+
 import obsidia.utilities.Coordinates;
 
 public abstract class AbstractCell implements Cells {
 
 	private Coordinates xy;
 	private final String ownerID;
+	public static final Path PATH = Path.of("data");
 	
 	public AbstractCell(final String ownerID, final Coordinates xy) {
 		this.ownerID = ownerID;
@@ -27,4 +30,6 @@ public abstract class AbstractCell implements Cells {
 	public void setCoordinates(Coordinates pos) {
 		this.xy = pos;
 	}
+	
+	
 }
