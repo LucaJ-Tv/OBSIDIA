22c805c92cb9b6f2c42df325bb1a1d03d9f1a3a8
Tella <tellarinipietro@gmail.com>
2022-06-23 01:58:12 +0200

    Update CoinManager.java

--- src/logic/entity/CoinManager.java
+++ src/logic/entity/CoinManager.java
@@ -1,12 +1,29 @@
 package logic.entity;
 
+import obsidia.entities.buildings.Farm;
 import obsidia.map.Cells;
+import obsidia.map.UseMap;
+import obsidia.players.PlayerList;
 
 
 public class CoinManager {
 	
-	protected boolean enoughCoins(Cells entity, int coin) {
-		return coin > entity.getCost();
+	private final PlayerList ply = new PlayerList();
+	private final UseMap map = new UseMap();
+	private final FarmManager farm = new FarmManager();
+	
+	protected boolean enoughCoins(Cells entity) {
+		if (entity instanceof Farm) {
+			return ply.getCoins() >= farm.getCost(ply.getName());
+		} else {
+			return ply.getCoins() >= entity.getCost();			
+		}
 	}
+	
+	protected void coinTurnUpdate() {
+		ply.addCoins(map.getIdBalance(ply.getName()));
+	}
+	
+	
 	
 }
