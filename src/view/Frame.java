00004d2391ff4de0788e2ced7da4371805f78dbb
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-22 17:46:33 +0200

    Update Frame.java
    add removePanel

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -13,22 +13,26 @@ public class Frame extends JFrame{
 	private final int PROPORTION_D = 4;
 
 	public Frame() {
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setTitle("OBSIDIA");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("OBSIDIA");
 		
 		//set dimension and position of the frame
 		final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		final int sw = (int) screen.getWidth();
 		final int sh = (int) screen.getHeight();
-		this.setSize(sw * PROPORTION_M / PROPORTION_D, sh * PROPORTION_M / PROPORTION_D);
-		this.setLocationRelativeTo(null);
-		this.setMaximumSize(screen);
-		this.setMinimumSize(new Dimension(sw / 2, sh / 2));
+		setSize(sw * PROPORTION_M / PROPORTION_D, sh * PROPORTION_M / PROPORTION_D);
+		setLocationRelativeTo(null);
+		setMaximumSize(screen);
+		setMinimumSize(new Dimension(sw / 2, sh / 2));
 		
-		this.setVisible(true);
+		setLocationRelativeTo(null);
 	}
 	
 	public void addPanel(JPanel panel, String position) {
 		this.add(panel,position);
+	}
+	
+	public void removePanel(JPanel panel){
+		this.remove(panel);
 	}
 }
