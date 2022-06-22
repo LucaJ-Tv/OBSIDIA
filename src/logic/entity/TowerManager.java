3288d86b8ef1fb532226f7927f8240d857a4c3a8
Tella <tellarinipietro@gmail.com>
2022-06-22 12:04:31 +0200

    Create TowerManager.java

---
+++ src/logic/entity/TowerManager.java
@@ -0,0 +1,20 @@
+package logic.entity;
+
+import obsidia.entities.cells.FreeCell;
+import obsidia.entities.towers.TowerOne;
+import obsidia.entities.towers.TowerTwo;
+import obsidia.entities.towers.Towers;
+import obsidia.map.Cells;
+
+public class TowerManager {
+
+	protected Towers newTower(Cells oldEntity)	{
+		Towers tower = null;
+		if (oldEntity instanceof FreeCell) {
+			tower = new TowerOne(oldEntity.getOwner(), oldEntity.getCoordinates());			
+		} else if (oldEntity instanceof TowerOne) {
+			tower = new TowerTwo(oldEntity.getOwner(), oldEntity.getCoordinates());	
+		}
+		return tower;
+	}
+}
