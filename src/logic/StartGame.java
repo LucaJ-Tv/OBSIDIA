7d0ccea9101f0d143df9724b7215ffe2d87d9951
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-23 11:58:09 +0200

    Update StartGame.java
    create Frame here
    pass the frame into the constructor of StartPanel

--- src/logic/StartGame.java
+++ src/logic/StartGame.java
@@ -1,11 +1,14 @@
 package logic;
 
+import view.Frame;
 import view.StartPanel;
 
 public class StartGame {
 
+	private static Frame frame = new Frame();
+	
 	public static void main(String[] args) {
-		new StartPanel();
+		new StartPanel(frame);
 	}
 
 }
