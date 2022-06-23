ffafa24d35c53d555be5da632284e5aa48e5a59d
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-23 15:02:46 +0200

    Update TurnGUI.java
    remove the inner class initPanel

--- src/view/TurnGUI.java
+++ src/view/TurnGUI.java
@@ -7,25 +7,23 @@ import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import logic.entity.EntityManager;
-import obsidia.entities.buildings.Farm;
-import obsidia.entities.towers.Towers;
-import obsidia.entities.troops.Troops;
+import logic.entity.Manager;
 import obsidia.map.DefaultMap;
 import obsidia.map.UseMap;
 import obsidia.utilities.Coordinates;
 
 public class TurnGUI {
 
-	private Frame turnFrame = new Frame();
 	private final Map<JButton,Coordinates> cells = new HashMap<>();
-	private EntityManager logicManager;
+	private Manager logicManager;
+	//TODO: next take height and width
+	UseMap map = new UseMap();
 	
 	//JPanel
 	JPanel mapPanel = new JPanel();
@@ -40,68 +38,75 @@ public class TurnGUI {
 	
 	//JLabel
 	private JLabel moneyLabel = new JLabel("MONEY:");
+	//TODO: get the money from the method in CoinManager
 	private JLabel money = new JLabel("10");
 	private JLabel balanceLabel = new JLabel("BALANCE:");
+	//TODO: get the balance from the method in CoinManager
 	private JLabel balance = new JLabel("0");
 	
-	public TurnGUI() {
-		UseMap map = new UseMap();
-		new DefaultMap("Map2");
-		
-		turnFrame.add(new initPanel(map.getHeight(),map.getWidth()));
-		turnFrame.setLocationRelativeTo(null);
-		turnFrame.setVisible(true);
-	}
+	//ActionListener of map cells
+	ActionListener actionL;
 	
+	public TurnGUI(Frame frame) {
+		new DefaultMap("Map2");
+		frame.setFrameLayout(new BorderLayout());
+		frame.revalidate();
+
+		southPanel.setBackground(Color.DARK_GRAY);
+
+		mapPanel.setLayout(new GridLayout(map.getHeight(),map.getWidth()));
+		
+        for (int i=0; i<map.getHeight(); i++){
+            for (int j=0; j<map.getWidth(); j++){
+                final JButton jb = new JButton(" X ");
+                jb.setBackground(Color.LIGHT_GRAY);
+                jb.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+                cells.put(jb, new Coordinates(i,j));
+                mapPanel.add(jb);
+                jb.addActionListener(actionL);
+            }
+        }
+        
+        troop.setBackground(Color.WHITE);
+        tower.setBackground(Color.WHITE);
+        farm.setBackground(Color.WHITE);
+        skip.setBackground(Color.WHITE);
+        exit.setBackground(Color.WHITE);
+        moneyLabel.setForeground(Color.WHITE);
+        money.setForeground(Color.WHITE);
+        balanceLabel.setForeground(Color.WHITE);
+        balance.setForeground(Color.WHITE);
+        //troop.setEnabled(false);
+        //tower.setEnabled(false);
+        //farm.setEnabled(false);
+        southPanel.add(troop);
+        southPanel.add(tower);
+        southPanel.add(farm);
+        southPanel.add(skip);
+        southPanel.add(exit);
+        southPanel.add(moneyLabel);
+        southPanel.add(money);
+        southPanel.add(balanceLabel);
+        southPanel.add(balance);
+        
+        frame.addComponent(mapPanel,BorderLayout.CENTER);
+        frame.addComponent(southPanel,BorderLayout.SOUTH);
+        
+        actionL = e -> {
+    		var position = cells.get(e.getSource());
+    		System.out.println(position);
+    	};
+    	
+    	frame.setVisible(true);
+	}
+		
+}
+	/*
 	@SuppressWarnings("serial")
 	public class initPanel extends JPanel{
-		
-		public initPanel(int height, int width) {
-			setLayout(new BorderLayout());
-			southPanel.setBackground(Color.DARK_GRAY);
 
-			mapPanel.setLayout(new GridLayout(height,width));
-			
-	        for (int i=0; i<height; i++){
-	            for (int j=0; j<width; j++){
-	                final JButton jb = new JButton(" X ");
-	                jb.setBackground(Color.LIGHT_GRAY);
-	                cells.put(jb, new Coordinates(i,j));
-	                mapPanel.add(jb);
-	                jb.addActionListener(actionL);
-	            }
-	        }
-	        
-	        troop.setBackground(Color.WHITE);
-	        tower.setBackground(Color.WHITE);
-	        farm.setBackground(Color.WHITE);
-	        skip.setBackground(Color.WHITE);
-	        exit.setBackground(Color.WHITE);
-	        moneyLabel.setForeground(Color.WHITE);
-	        money.setForeground(Color.WHITE);
-	        balanceLabel.setForeground(Color.WHITE);
-	        balance.setForeground(Color.WHITE);
-	        southPanel.add(troop);
-	        southPanel.add(tower);
-	        southPanel.add(farm);
-	        southPanel.add(skip);
-	        southPanel.add(exit);
-	        southPanel.add(moneyLabel);
-	        southPanel.add(money);
-	        southPanel.add(balanceLabel);
-	        southPanel.add(balance);
-	        
-	        add(mapPanel,BorderLayout.CENTER);
-	        add(southPanel,BorderLayout.SOUTH);
-		}
-			
-	}
 	
-	ActionListener actionL = e -> {
-		var position = cells.get(e.getSource());
-		this.logicManager.OnPress(position);
-		updateButton();
-	};
+	
 	
 	public void setOnTroop(Troops c) {
 		troop.setEnabled(true);
@@ -156,6 +161,7 @@ public class TurnGUI {
 	private void updateButton() {
 		southPanel.validate();
 	}
+	
 
 
-}
+}*/
