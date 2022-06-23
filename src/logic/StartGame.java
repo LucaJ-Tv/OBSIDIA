6a2cd64321da79b80f99907e263e0ee4bb41c0dc
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-23 17:47:55 +0200

    Update StartGame.java
    add possibility to restart the same game at the end of a game

--- src/logic/StartGame.java
+++ src/logic/StartGame.java
@@ -1,14 +1,26 @@
 package logic;
 
+import obsidia.map.UseMap;
+import obsidia.players.PlayerList;
 import view.Frame;
 import view.StartPanel;
 
 public class StartGame {
 	
 	private static Frame frame = new Frame();
-
+	private static PlayerList ply = new PlayerList();
+	private static UseMap map = new UseMap();
+	
 	public static void main(String[] args) {
-		new StartPanel(frame);
+		new StartGame(true);
 	}
-
+	
+	public StartGame(boolean cleanPlayerList) {
+		if(cleanPlayerList) {
+			new StartPanel(frame);
+			ply.removeAllPlayer();
+		}
+		map.cleanMap();
+	}
+	
 }
