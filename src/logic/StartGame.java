8ab9ec5b993157baf23c54bad99b8742491784a6
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-23 15:00:29 +0200

    No change

--- src/logic/StartGame.java
+++ src/logic/StartGame.java
@@ -4,9 +4,9 @@ import view.Frame;
 import view.StartPanel;
 
 public class StartGame {
-
-	private static Frame frame = new Frame();
 	
+	private static Frame frame = new Frame();
+
 	public static void main(String[] args) {
 		new StartPanel(frame);
 	}
