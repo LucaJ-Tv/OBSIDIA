fc9b6f0077a8023ce0d27c555bd414768f1e4312
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-24 10:35:34 +0200

    Important change StartGame.java
    inizialise all not in costructor but in method inizGame
    inizialise listColor whit color and then shuffle the list
    add clearFrame which between a change screen clean the frame
    add moveTurn switch between StartPanel and TurnPanel
    add exit
    add getColor which return a color of listColor

--- src/logic/StartGame.java
+++ src/logic/StartGame.java
@@ -1,5 +1,14 @@
 package logic;
 
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.swing.JOptionPane;
+
+import obsidia.map.DefaultMap;
 import obsidia.map.UseMap;
 import obsidia.players.PlayerList;
 import view.Frame;
@@ -12,18 +21,58 @@ public class StartGame {
 	private static PlayerList ply = new PlayerList();
 	private static UseMap map = new UseMap();
 	
+	private final List<Color> listColor = Arrays.asList(
+		new Color(200,15,15),
+		new Color(200,15,150),
+		new Color(50,40,235),
+		new Color(30,210,210),
+		new Color(10,200,30),
+		new Color(240,250,20),
+		new Color(250,150,30)
+		);
+	
 	public static void main(String[] args) {
-		new StartGame(true);
+		new StartGame().inizGame(true);
 	}
 	
-	public StartGame(boolean cleanPlayerList) {
+	public void inizGame(boolean cleanPlayerList) {
+		//if you want restart with the same player pass cleanPlayerList = false
 		if(cleanPlayerList) {
-			new StartPanel(frame);
+			clearFrame();
+			Collections.shuffle(listColor);
 			ply.removeAllPlayer();
+			new StartPanel(frame);
 		}else {
-			new TurnPanel(frame);
+			moveTurn("Map2");
 		}
 		map.cleanMap();
+	}
+	
+	//between a change screen clean the frame
+	private void clearFrame() {
+		frame.setVisible(false);
+		frame.getContentPane().removeAll();
+	}
+	
+	//StartPanel -> TurnPanel
+	public void moveTurn(String map) {
+		clearFrame();
+		new DefaultMap(map);
+		new TurnPanel(frame);
+	}
+
+	public void exit() {
+		if(JOptionPane.showConfirmDialog(
+    			frame,
+    			"Are you sure to exit?",
+    			"EXIT",
+    			JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) 
+    		System.exit(0);
+	}
+	
+	
+	public Color getColor(int pos) throws NoSuchElementException{
+		return listColor.get(pos);
 	}
 	
 }
