e992492dcfb2b7d5365fd852793b72e057314a54
Tella <tellarinipietro@gmail.com>
2022-06-24 19:35:49 +0200

    Create documentation of Player.java

--- src/obsidia/players/Player.java
+++ src/obsidia/players/Player.java
@@ -2,6 +2,16 @@ package obsidia.players;
 
 import java.awt.Color;
 
+/**
+ * 
+ * @author Tellarini Pietro
+ * 
+ * The class is used to store a player's information
+ * All it's method are used only to provide information 
+ * and set variable by PlayerList.class
+ * @see PlayerList.java
+ *
+ */
 public class Player{
 	
 	private static final int DEFAULT_COINS = 10;
@@ -17,6 +27,13 @@ public class Player{
 		this.coins = startCoins;	
 	}
 	
+	/**
+	 * 
+	 * @param player
+	 * @param color
+	 * This constuctor will assign at variable conins the DEFAULT_COINS value = 10
+	 * 
+	 */
 	public Player(final String player, final Color color) {
 		this(player, color, Player.DEFAULT_COINS);
 	}
