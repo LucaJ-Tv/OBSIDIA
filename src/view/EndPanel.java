e6b793ea26d118348e4e62c44c5d57018246c4b9
Tella <tellarinipietro@gmail.com>
2022-06-24 12:07:45 +0200

    Create EndPanel.java

---
+++ src/view/EndPanel.java
@@ -0,0 +1,115 @@
+package view;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import logic.Manager;
+import logic.StartGame;
+
+public class EndPanel {
+	
+	private final Frame frame ;
+	private Manager logic;
+	private GridBagConstraints gbc = new GridBagConstraints();
+	
+	public EndPanel(Frame frame) {
+		this.frame = frame;
+		setFrame();		
+	}
+	
+	private void setFrame() {
+		
+		this.frame.setVisible(false);
+		this.frame.getContentPane().removeAll();
+		this.frame.setFrameLayout(new GridBagLayout());
+		
+		gbc.weighty = 1;
+		
+		addTitLabel();
+		addWinPanel();
+		addButPanel();
+		
+		this.frame.validate();
+		this.frame.setVisible(true);
+	}
+	
+	private void addTitLabel() {
+		
+		Label titLabel = new Label("OBSIDIA", 80);
+        
+		this.gbc.anchor = GridBagConstraints.NORTH;
+		this.gbc.gridwidth = GridBagConstraints.REMAINDER;
+        this.frame.addComponent(titLabel, gbc);
+	}
+	
+	
+	private void addWinPanel() {
+		
+		JPanel winPanel = new JPanel();
+		Label winLabel;
+
+		winLabel = new Label(logic.winner(), 60);
+		winPanel.add(winLabel);
+		
+		winLabel = new Label(" WIN!", 60);
+		winPanel.add(winLabel);
+		
+		winPanel.setBackground(Color.yellow);
+		
+		this.gbc.anchor = GridBagConstraints.CENTER;
+		this.gbc.gridwidth = GridBagConstraints.REMAINDER;
+		this.frame.addComponent(winPanel, gbc);
+		
+	}
+	
+	private void addButPanel() {
+
+		JPanel butPanel = new JPanel();
+	    Button but;
+	     
+	    but = new Button("Play Again");
+		butPanel.add(but);
+		playAgainPerformer(but);
+		
+		but = new Button("New Game");
+		butPanel.add(but);
+		newGamePerformer(but);
+		
+		but = new Button("Exit");
+		butPanel.add(but);
+		exitPerformer(but);
+		
+		this.gbc.anchor = GridBagConstraints.SOUTH;
+		frame.addComponent(butPanel, gbc);
+		
+	}
+	
+	private void playAgainPerformer(Button plyAgain) {
+		plyAgain.addActionListener(e -> {
+			new StartGame(false);
+		});
+	}
+	
+	private void newGamePerformer(Button newGame) {
+		newGame.addActionListener(e ->{
+			new StartGame(true);
+		});
+	}
+	
+	private void exitPerformer(Button exit) {
+		exit.addActionListener(e ->{
+			if(JOptionPane.showConfirmDialog(
+        			frame,
+        			"Are you sure to exit?",
+        			"EXIT",
+        			JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) 
+        		System.exit(0);
+		});
+	}
+	
+
+}
