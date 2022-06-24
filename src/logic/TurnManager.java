2a9e87acd0bf87b90a18c108929df532c422e547
LucaBighini <80529975+LucaBighini@users.noreply.github.com>
2022-06-24 11:50:54 +0200

    Update TurnManager.java
    add mapWidth and mapHeight to get dimension from Usemap and pass to view

--- src/logic/TurnManager.java
+++ src/logic/TurnManager.java
@@ -1,5 +1,16 @@
 package logic;
 
+import obsidia.map.UseMap;
+
 public class TurnManager {
 	
+	UseMap map = new UseMap();
+	
+	public int mapWidth() {
+		return map.getWidth();
+	}
+	
+	public int mapHeight() {
+		return map.getHeight();
+	}
 }
