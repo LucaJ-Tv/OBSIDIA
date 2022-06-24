467420dbff14062aab09ef5c3831679befea35a7
Tella <tellarinipietro@gmail.com>
2022-06-24 15:22:28 +0200

    Update MapManager.java

--- src/logic/game/MapManager.java
+++ src/logic/game/MapManager.java
@@ -1,13 +1,39 @@
 package logic.game;
 
+import javax.swing.ImageIcon;
+
 import logic.ManagerImplementation;
 import obsidia.map.DefaultMap;
+import obsidia.utilities.Coordinates;
 
 public class MapManager extends ManagerImplementation{
+	
+	DefaultMap def = new DefaultMap();
 
-	public void loadMap() {
-		new DefaultMap("Map1");
+	@Override
+	public void defaultMapLoad(String file) {
+		def.setMap(file);
+		def.loadMap();
+		def.createMap();
 	}
 
+	@Override
+	public String[] defaulMapGive() {
+		return def.listMap();
+	}
+	
+	@Override
+	public void cleanMap() {
+		ManagerImplementation.map
+		.cleanMap();
+	}
+	
+	
+	@Override
+	public ImageIcon iconCell(Coordinates pos) {
+		return 	ManagerImplementation.map
+				.getEntity(pos)
+				.getIcon();
+	}
 }
 
