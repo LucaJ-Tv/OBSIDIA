805da3381d1641b54726ad655895a9220823a4ce
Tella <tellarinipietro@gmail.com>
2022-06-24 19:36:22 +0200

    Documentation of Coordinates.java

--- src/obsidia/utilities/Coordinates.java
+++ src/obsidia/utilities/Coordinates.java
@@ -2,6 +2,14 @@ package obsidia.utilities;
 
 import java.util.Set;
 
+/**
+ * 
+ * @author Tellarini Pietro
+ * 
+ * the class provides the positions of the cells in the map 
+ * that mirror the positions in the graphical interface.
+ * 
+ */
 public class Coordinates {
 	private final int x;
 	private final int y;
@@ -11,9 +19,6 @@ public class Coordinates {
 		this.y = y;
 	}
 	
-	public int getKey()	{
-		return this.x*100+this.y;
-	}
 	public int getX() {
 		return this.x;
 	}
@@ -26,10 +31,25 @@ public class Coordinates {
 		return "x: " + this.x  + "y: " + this.y;
 	}
 	
+	/**
+	 * @return an Integer key used to access in the map (Hash map)
+	 */
+	public int getKey()	{
+		return this.x*100+this.y;
+	}
+	
+	/**
+	 * @param p2 another Coordinate
+	 * @return true if coordinates has the same values
+	 */
 	public boolean samePosition(Coordinates p2) {
 		return this.getX() == p2.getX() && this.getY() == p2.getY();
 	}
 	
+	/**
+	 * @param other another Coordinate
+	 * @return the distance between one Coordinate and given one
+	 */
 	public int distance(final Coordinates other) {
 		final int Dx = Math.abs(other.getX() - this.getX());
 		final int Dy = Math.abs(other.getY() - this.getY());
@@ -40,6 +60,12 @@ public class Coordinates {
 			return Dy;
 	}
 	
+	/**
+	 * @param distance radius from Coordinate
+	 * @param mapWidth width of the map
+	 * @param mapHeight height of the map
+	 * @return the coordinates near the coordinate at given distance in the map
+	 */
 	public Coordinates[] near(final int distance, final int mapWidth, final int mapHeight) {
 		
 		Set<Integer> set = Set.of(0);
