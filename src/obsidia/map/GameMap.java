e2af80ec54ed220a4119a9c9cb79af84b531c358
Tella <tellarinipietro@gmail.com>
2022-06-24 20:26:53 +0200

    Documentation GameMap.java

--- src/obsidia/map/GameMap.java
+++ src/obsidia/map/GameMap.java
@@ -7,12 +7,28 @@ import obsidia.entities.buildings.Farm;
 import obsidia.entities.cells.FreeCell;
 import obsidia.utilities.Coordinates;
 
+/**
+ * 
+ * @author Tellarini Pietro
+ *
+ * The class creates and manages the game map. 
+ * The map is a hash map where the key is an integer 
+ * while the value it's a Cell
+ * @see Cells.java
+ * @see Coordinates.java
+ * 
+ */
 public class GameMap {
 	
 	private static Map<Integer, Cells> MAP = new HashMap<Integer, Cells>();
 	private static int HEIGHT = 0;
 	private static int WIDTH = 0;
 	
+	/** 
+	 * @param height of the map (matrix)
+	 * @param width of the map (matrix)
+	 * Set dimension of the map	
+	 */
 	public void setDimension(final int height, final int width) {
 		GameMap.HEIGHT = height;
 		GameMap.WIDTH = width;
@@ -26,33 +42,54 @@ public class GameMap {
 		return GameMap.HEIGHT;
 	}
 	
+	/**
+	 * @param entity it's a Cells (troup, castle, tower, farm, FreeCell)
+	 * Add an entity at the map with the given key
+	 */
 	public void addEntity(final Cells entity) {
 		GameMap.MAP.put(entity.getCoordinates().getKey(), entity);
 	}
 	
-	//TODO that's not completely correct, tecnically only troup colud change position so
-	// the field position in Abstract cell shuld be final (but not for troop....)
+	/**
+	 * @param entity
+	 * @param pos
+	 * Change the position of the new entity 
+	 * an puts it into the map
+	 */
 	public void moveEntity(final Cells entity, Coordinates pos) {
 		entity.setCoordinates(pos);
 		GameMap.MAP.replace(pos.getKey(), entity);
 	}
 	
+	/**
+	 * @param xy Coordinates
+	 * @return Defence of the cell at given position
+	 */
 	public int getDefence(final Coordinates xy) {
 		return GameMap.MAP.get(xy.getKey()).getDefence();
 	}
 
+	/**
+	 * @param xy Coordinates
+	 * @return name of Player's cell at given position
+	 */
 	public String getOwner(final Coordinates xy) {
 		return GameMap.MAP.get(xy.getKey()).getOwner();
 	}
 	
+	/**
+	 * @param xy Coordinates
+	 * @return The cell at given position
+	 */
 	public Cells getEntity(final Coordinates xy) {
 		return GameMap.MAP.get(xy.getKey());
 	}
 	
-	public int getBalance(final Coordinates xy) {
-		return GameMap.MAP.get(xy.getKey()).getBalance();
-	}
-	
+	/**
+	 * @param ID name of Player
+	 * Remove all the cell of a player and replace them 
+	 * with a FreeCell with no Owner
+	 */
 	public void removePlayer(final String ID) {
 		for (var i : GameMap.MAP.values()) {
 			if(i.getOwner() == ID) {
@@ -61,6 +98,10 @@ public class GameMap {
 		}
 	}
 	
+	/**
+	 * @param ID name of Player
+	 * @return the number of farms of a Player built in the map
+	 */
 	public int numberFarm(final String ID) {
 		int num = 0;
 		for ( final Cells i : GameMap.MAP.values() ) {
@@ -71,6 +112,10 @@ public class GameMap {
 		return num;		
 	}
 	
+	/**
+	 * @param ID name of Player
+	 * @return the balance of the Cells of a Player
+	 */
 	public int getIdBalance(final String ID){
 		int balance = 0;
 		for ( final var i : GameMap.MAP.values() ) {
@@ -81,6 +126,9 @@ public class GameMap {
 		return balance;
 	}
 	
+	/**
+	 * Remove everything from the map (map clear)
+	 */
 	public void cleanMap() {
 		MAP.clear();
 	}
