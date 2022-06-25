f34bbb73c8b87eab7093a879e0e39e7a0859113b
Tella <tellarinipietro@gmail.com>
2022-06-25 19:25:59 +0200

    Update Troops.java

--- src/obsidia/entities/troops/Troops.java
+++ src/obsidia/entities/troops/Troops.java
@@ -2,12 +2,29 @@ package obsidia.entities.troops;
 
 import obsidia.map.Cells;
 
+/**
+ * 
+ * The interface instantiates methods common to all troops.
+ * It is also useful for distinguishing the various types of entities in the map.
+ * 
+ * @author Tellarini Pietro
+ *
+ */
 public interface Troops extends Cells {
 	
+	/** 
+	 * @return movement range of a troop in the map
+	 */
 	public int getMovement();
 	
+	/**
+	 * @return attack level of a troop
+	 */
 	public int getOffence();
 	
+	/**
+	 * @return Troop Level
+	 */
 	public int getLevel();
 	
 }
