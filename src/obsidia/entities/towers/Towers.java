f3ff8cf90bda58ceba480b873ed4422b3a726830
Tella <tellarinipietro@gmail.com>
2022-06-25 19:25:40 +0200

    Update Towers.java

--- src/obsidia/entities/towers/Towers.java
+++ src/obsidia/entities/towers/Towers.java
@@ -2,8 +2,19 @@ package obsidia.entities.towers;
 
 import obsidia.map.Cells;
 
+/**
+ * 
+ * The interface instantiates methods common to all towers.
+ * It is also useful for distinguishing the various types of entities in the map.
+ * 
+ * @author Tellarini Pietro
+ *
+ */
 public interface Towers extends Cells {
 	
+	/**
+	 * @return integer of tower's level
+	 */
 	public int getLevel();
 	
 }
